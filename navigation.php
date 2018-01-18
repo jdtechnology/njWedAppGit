ea365e3aa36d5fc0106ecc9f8427f70bca4fbeeb
Jack Fisher <jackfisher2012@gmail.com>
2018-01-18 11:31:43 +0000

    Fix typo in navigation, add padding to p of main

--- navigation.php
+++ navigation.php
@@ -2,7 +2,7 @@
 	<a class="ajax-nav" href="home">Home</a>
 	<!-- <a class="ajax-nav" href="home"><img src="includes/images/bg-icon.png" style="height:30px"></a> -->
 	<a class="ajax-nav" href="ootd">Order of the day</a>
-	<a class="ajav-nav" href="information">Infromation</a>
+	<a class="ajav-nav" href="information">Information</a>
 	<a class="ajax-nav" href="registry">Registry</a>
 	<a class="ajax-nav" href="rsvp">RSVP</a>
 	<a href="javascript:void(0);" id="navBarToggle" class="icon">&#9776;</a>
